aca716af66943806073c96d3406fba930d8904d7
chavemar <chavemar@oregonstate.edu>
2021-03-04 20:05:42 -0800

    template for test suite and functions

---
+++ task.py
@@ -0,0 +1,12 @@
+
+
+def conv_num(num_str):
+    return True
+
+
+def my_datetime(num_sec):
+    return True
+
+
+def conv_endian(num, endian='big'):
+    return True
--- tests.py
+++ tests.py
@@ -1,10 +1,25 @@
 import unittest
+from task import conv_num
+from task import my_datetime
+from task import conv_endian
 
 
-class TestCase(unittest.TestCase):
+class TestCase_ConvNum(unittest.TestCase):
 
     def test1(self):
-        self.assertTrue(True)
+        self.assertTrue(conv_num(str(123)))
+
+
+class TestCase_MyDateTime(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(my_datetime(123))
+
+
+class TestCase_ConvEndian(unittest.TestCase):
+
+    def test1(self):
+        self.assertTrue(conv_endian(123))
 
 
 if __name__ == '__main__':
