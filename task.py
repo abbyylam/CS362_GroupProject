805d251eac12a90cd99587c77618378203210a54
chavemar <chavemar@oregonstate.edu>
2021-03-05 18:14:27 -0800

    implemented conv_endian function & its test suite

--- task.py
+++ task.py
@@ -9,4 +9,81 @@ def my_datetime(num_sec):
 
 
 def conv_endian(num, endian='big'):
-    return True
+    # validate input arguements
+    assert type(num) == int, "Incorrect input"
+    assert type(endian) == str, "Incorrect input"
+
+    negativeNum = False
+
+    # variables to store the working hex code,
+    # and the output string,
+    # and the bytes in a list in big endian order
+    workingHex = []
+    outputStr = ''
+    bigEndianByteList = []
+
+    # remove minus sign if negative number, set negativeNum
+    if num < 0:
+        negativeNum = True
+        num = str(num)
+        num = int(num[1:])
+
+    # return '00' byte
+    if num == 0:
+        return '00'
+
+    while(num != 0):
+        # store the remainder
+        tmpRemainder = num % 16
+
+        # append a alphanumeric or alphabetic character
+        if(tmpRemainder < 10):
+            workingHex.append(chr(tmpRemainder + 48))
+        else:
+            workingHex.append(chr(tmpRemainder + 55))
+
+        # get rid off least significant value
+        num = int(num / 16)
+
+    # append a zero if odd numbered nibbles in list
+    if len(workingHex) % 2 == 1:
+        workingHex.append('0')
+
+    # counters and formmating string variables
+    i = len(workingHex) - 1
+    charsInString = 0
+    currentByteStr = ''
+    while(i >= 0):
+        # add a space after every byte but not before the first byte
+        if (charsInString % 2 == 0) and outputStr != '':
+            # add space character after byte
+            outputStr = outputStr + ' '
+            bigEndianByteList.append(currentByteStr)
+            # reset byte string
+            currentByteStr = ''
+        outputStr = outputStr + workingHex[i]
+        currentByteStr = currentByteStr + workingHex[i]
+        charsInString = charsInString + 1
+        i = i - 1
+
+    # append last byte
+    bigEndianByteList.append(currentByteStr)
+
+    if endian == 'big':
+        return make_neg(outputStr) if negativeNum else outputStr
+    elif endian == 'little':
+        outputStr = ''
+        littleEndianByteList = bigEndianByteList[::-1]
+        # reorder outputStr form big to little endian
+        for byte in littleEndianByteList:
+            outputStr = outputStr + byte
+            outputStr = outputStr + ' '
+        # remove final space characater
+        outputStr = outputStr[:-1]
+        return make_neg(outputStr) if negativeNum else outputStr
+    else:
+        return None
+
+
+def make_neg(arg_str):
+    return '-' + arg_str
--- tests.py
+++ tests.py
@@ -18,8 +18,43 @@ class TestCase_MyDateTime(unittest.TestCase):
 
 class TestCase_ConvEndian(unittest.TestCase):
 
+    message = 'Unexpected result!'
+
     def test1(self):
-        self.assertTrue(conv_endian(123))
+        expected = '0E 91 A2'
+        self.assertEqual(expected, conv_endian(954786, 'big'), self.message)
+
+    def test2(self):
+        expected = '0E 91 A2'
+        self.assertEqual(expected, conv_endian(954786), self.message)
+
+    def test3(self):
+        expected = '-0E 91 A2'
+        self.assertEqual(expected, conv_endian(-954786), self.message)
+
+    def test4(self):
+        expected = 'A2 91 0E'
+        self.assertEqual(expected, conv_endian(954786, 'little'), self.message)
+
+    def test5(self):
+        expected = '-A2 91 0E'
+        self.assertEqual(expected, conv_endian(-954786, 'little'),
+                         self.message)
+
+    def test6(self):
+        expected = '-A2 91 0E'
+        self.assertEqual(expected, conv_endian(num=-954786, endian='little'),
+                         self.message)
+
+    def test7(self):
+        expected = None
+        self.assertEqual(expected, conv_endian(num=-954786, endian='small'),
+                         self.message)
+
+    def test8(self):
+        expected = '00'
+        self.assertEqual(expected, conv_endian(num=0, endian='big'),
+                         self.message)
 
 
 if __name__ == '__main__':
