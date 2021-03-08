09268bedb60656ccc45b6b49a500f02a80bdb0a9
jawkneefence <34426199+jawkneefence@users.noreply.github.com>
2021-03-07 23:03:47 -0800

    Test cases & Syntax fixes for entire file

--- tests.py
+++ tests.py
@@ -4,19 +4,50 @@ from task import my_datetime
 from task import conv_endian
 
 
-class TestCase_ConvNum(unittest.TestCase):
+class TestCaseConvNum(unittest.TestCase):
+
+    message = 'Unexpected result!'
 
     def test1(self):
-        self.assertTrue(conv_num(str(123)))
+        expected = 12345
+        self.assertEqual(expected, conv_num('12345'), self.message)
+
+    def test2(self):
+        expected = -123.45
+        self.assertEqual(expected, conv_num('-123.45'), self.message)
+
+    def test3(self):
+        expected = 0.45
+        self.assertEqual(expected, conv_num('.45'), self.message)
+
+    def test4(self):
+        expected = 123.0
+        self.assertEqual(expected, conv_num('123.'), self.message)
+
+    def test5(self):
+        expected = 2772
+        self.assertEqual(expected, conv_num('0xAD4'), self.message)
+
+    def test6(self):
+        expected = None
+        self.assertEqual(expected, conv_num('0xAZ4'), self.message)
+
+    def test7(self):
+        expected = None
+        self.assertEqual(expected, conv_num('12345A'), self.message)
+
+    def test8(self):
+        expected = None
+        self.assertEqual(expected, conv_num('12.3.45'), self.message)
 
 
-class TestCase_MyDateTime(unittest.TestCase):
+class TestCaseMyDateTime(unittest.TestCase):
 
     def test1(self):
         self.assertTrue(my_datetime(123))
 
 
-class TestCase_ConvEndian(unittest.TestCase):
+class TestCaseConvEndian(unittest.TestCase):
 
     message = 'Unexpected result!'
 
@@ -38,23 +69,19 @@ class TestCase_ConvEndian(unittest.TestCase):
 
     def test5(self):
         expected = '-A2 91 0E'
-        self.assertEqual(expected, conv_endian(-954786, 'little'),
-                         self.message)
+        self.assertEqual(expected, conv_endian(-954786, 'little'), self.message)
 
     def test6(self):
         expected = '-A2 91 0E'
-        self.assertEqual(expected, conv_endian(num=-954786, endian='little'),
-                         self.message)
+        self.assertEqual(expected, conv_endian(num=-954786, endian='little'), self.message)
 
     def test7(self):
         expected = None
-        self.assertEqual(expected, conv_endian(num=-954786, endian='small'),
-                         self.message)
+        self.assertEqual(expected, conv_endian(num=-954786, endian='small'), self.message)
 
     def test8(self):
         expected = '00'
-        self.assertEqual(expected, conv_endian(num=0, endian='big'),
-                         self.message)
+        self.assertEqual(expected, conv_endian(num=0, endian='big'), self.message)
 
 
 if __name__ == '__main__':
